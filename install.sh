d1a66361acd9935653b3bb88bc4180e65cb285de
Scott Bennett <scott@fantag.live>
2020-06-12 17:05:27 -0700

    add .zshrc

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ apt-get install -y zsh
 echo zsh --version
 chsh -s $(which zsh)
 
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
