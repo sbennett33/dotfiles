fe76e294078ae59974eaf828c00bfb0ee6a335a1
Scott Bennett <scott@fantag.live>
2020-06-15 13:28:31 -0700

    add vscode tasks

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ apt-get install fasd
 
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/vscode/tasks.json $HOME/.config/Code/User/tasks.json
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
