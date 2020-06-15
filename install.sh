af6805a9688a728cff779676cd6a83fc627866ff
Scott Bennett <scott@fantag.live>
2020-06-15 20:45:03 +0000

    fix typo

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ apt-get update
 apt-get install -y zsh apt-utils
 
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/vscode/tasks.json $HOME/.config/Code/User/tasks.json
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
