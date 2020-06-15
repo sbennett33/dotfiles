1d9cbdb270be7f35992c0a7e7c01d7f25bdc7f4d
Scott Bennett <scott@fantag.live>
2020-06-15 16:27:52 +0000

    fix fasd install

--- install.sh
+++ install.sh
@@ -2,15 +2,13 @@
 
 echo "Setting up your dev environment..."
 
-apt-get install -y zsh apt-utils
-echo zsh --version
-chsh -s $(which zsh)
+apt-get update
+apt-get install -y zsh apt-utils software-properties-common
+
+add-apt-repository ppa:aacebedo/fasd
+apt-get install fasd
 
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-add-apt-repository ppa:aacebedo/fasd
-apt-get update
-apt-get install fasd
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
