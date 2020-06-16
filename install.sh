a056c3b70da784b921d1bfb2e5a0967dea7e2512
Scott Bennett <scott@fantag.live>
2020-06-16 09:08:15 -0700

    install vscode extensions

--- install.sh
+++ install.sh
@@ -8,4 +8,9 @@ apt-get install -y zsh apt-utils
 rm -rf $HOME/.zshrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
+code --install-extension will-wow.vscode-alternate-file
+code --install-extension github.vscode-pull-request-github
+code --install-extension eamodio.gitlens
+code --install-extension ms-vsliveshare.vsliveshare
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
