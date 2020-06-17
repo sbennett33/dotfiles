5dbfbae05c1d39d94477a0f30620be9aafbcef71
Scott Bennett <scott@fantag.live>
2020-06-17 15:45:05 +0000

    install vim

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Setting up your dev environment..."
 
 apt-get update
-apt-get install -y zsh apt-utils
+apt-get install -y zsh apt-utils vim
 
 rm -rf $HOME/.zshrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
