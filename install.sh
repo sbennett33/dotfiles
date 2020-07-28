a3ab77eff6558980bb0ed7ef42afb3e098932460
Scott Bennett <scott@fantag.live>
2020-07-28 08:45:21 -0700

    install newer git

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ apt-get install -y zsh apt-utils vim
 rm -rf $HOME/.zshrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
-code --install-extension will-wow.vscode-alternate-file
-code --install-extension github.vscode-pull-request-github
-code --install-extension eamodio.gitlens
-code --install-extension ms-vsliveshare.vsliveshare
+wget https://github.com/git/git/archive/v2.24.3.tar.gz
+tar -zxf v2.24.3.tar.gz
+cd git-2.24.3 && \
+    make prefix=/usr/local all && \
+    make prefix=/usr/local install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
